3f4e634772586d24093034b168b76ce9144c9ffd
Torrey Adams <torrey.adams@gmail.com>
2026-05-23 18:43:05 -0700

    feat(keystatic): update astro.config.mjs to hybrid mode with Keystatic and Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import keystatic from '@keystatic/astro';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  adapter: cloudflare(),
   site: 'https://quirgs.com',
-  integrations: [mdx()],
+  integrations: [mdx(), keystatic()],
 });
